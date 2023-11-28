77d7ca006d70c5bf6e4087783b591802219b4e99
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-11-28 10:43:14 +0100

    style

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1.5,
+        centerTitle: true,
+      ),
+    );
   }
 }
