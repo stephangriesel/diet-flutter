47fcb3bf7760a81605a11875aa210a8edaa0f3bc
Stephan Griesel <stephangriesel@peopleintouch.com>
2023-11-29 07:44:48 +0100

    insert model & format text

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,9 +1,12 @@
+import 'package:dietflutter/models/category_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  List<CategoryModel> categories = [];
 
   @override
   Widget build(BuildContext context) {
@@ -14,31 +17,29 @@ class HomePage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             _searchField(),
-            SizedBox(height: 40,),
+            SizedBox(
+              height: 40,
+            ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(left:20),
-                  child: Text(
-                    'Category',
-                    style: TextStyle(
-                      color:Colors.black,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600
-                    )
-                  ),
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Text('Category',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600)),
                 ),
-                SizedBox(height: 15,),
+                SizedBox(
+                  height: 15,
+                ),
                 Container(
-                  height: 150,
-                  color: Colors.green,
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
+                    height: 150,
+                    color: Colors.green,
+                    child: ListView.builder(itemBuilder: (context, index) {
                       return Container();
-                    }
-                  )
-                )
+                    }))
               ],
             )
           ],
@@ -47,53 +48,49 @@ class HomePage extends StatelessWidget {
 
   Container _searchField() {
     return Container(
-            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
-            decoration: BoxDecoration(boxShadow: [
-              BoxShadow(
-                color: Color(0xff1D1617).withOpacity(0.11),
-                blurRadius: 40,
-                spreadRadius: 0.0,
-              )
-            ]),
-            child: TextField(
-                decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: EdgeInsets.all(15),
-                    hintText: 'Search',
-                    hintStyle: TextStyle(
-                      color: Color(0xffDDDADA),
-                      fontSize: 14
-                    ),
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: SvgPicture.asset('assets/icons/Search.svg'),
-                    ),
-                    suffixIcon: Container(
-                      width: 100,
-                      child: IntrinsicHeight(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            VerticalDivider(
-                              color: Colors.black,
-                              indent: 10,
-                              endIndent: 10,
-                              thickness: 0.1,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child:
-                                  SvgPicture.asset('assets/icons/Filter.svg'),
-                            ),
-                          ],
-                        ),
+      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Color(0xff1D1617).withOpacity(0.11),
+          blurRadius: 40,
+          spreadRadius: 0.0,
+        )
+      ]),
+      child: TextField(
+          decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              contentPadding: EdgeInsets.all(15),
+              hintText: 'Search',
+              hintStyle: TextStyle(color: Color(0xffDDDADA), fontSize: 14),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg'),
+              ),
+              suffixIcon: Container(
+                width: 100,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      VerticalDivider(
+                        color: Colors.black,
+                        indent: 10,
+                        endIndent: 10,
+                        thickness: 0.1,
                       ),
-                    ),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15),
-                        borderSide: BorderSide.none))),
-          );
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SvgPicture.asset('assets/icons/Filter.svg'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none))),
+    );
   }
 
   AppBar appBar() {
